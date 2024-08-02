96d041669bc375d9511af0f7219005883a420c8a
Bidyut Hazra <bidyut.hrz74@gmail.com>
2024-08-02 22:12:37 +0530

    home & 404 added

--- server.js
+++ server.js
@@ -9,7 +9,14 @@ const SERVER_PORT = process.env.POSTS_SERVER_PORT || 3000
 
 app.use(express.json())
 
+app.get('/', (req, res) => {
+    res.json({ msg: `hello, world!` })
+})
 app.use('/api/v1/posts', ROUTER)
+
+app.use((req, res, next) => {
+    res.status(404).send('Sorry can\'t find that!');
+});
 
 const startServer = () => {
     app.listen(SERVER_PORT, () => console.log(`server is running on port: ${SERVER_PORT}`))
