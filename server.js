234939b52f02be59eed67a6e45c86f07ea609c7d
Bidyut Hazra <bidyut.hrz74@gmail.com>
2024-08-03 17:44:40 +0530

    log removed

--- server.js
+++ server.js
@@ -2,16 +2,12 @@ const express = require('express')
 const dotenv = require('dotenv')
 const ROUTER = require('./core/router')
 const connectDB = require('./config/database')
-const requestLogger = require('./middleware/requestLogger')
-const errorLogger = require('./middleware/errorLogger')
 
 dotenv.config()
 const app = express()
 const SERVER_PORT = process.env.POSTS_SERVER_PORT || 3000
 
 app.use(express.json())
-
-app.use(requestLogger)
 
 app.get('/', (req, res) => {
     res.json({ msg: `Welcome to the API. Please use the /api/v1 path to access API endpoints.` })
@@ -22,8 +18,6 @@ app.use('/api/v1', ROUTER)
 app.use((req, res, next) => {
     res.status(404).send('Sorry can\'t find that endpoints!');
 });
-
-app.use(errorLogger)
 
 const startServer = () => {
     app.listen(SERVER_PORT, () => console.log(`server is running on port: ${SERVER_PORT}`))
