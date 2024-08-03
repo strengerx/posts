56490fa3af2b1ff1329e7a3b5ea3eba8ed07588a
Bidyut Hazra <bidyut.hrz74@gmail.com>
2024-08-03 17:35:07 +0530

    changes added

--- utilities/addLogs.js
+++ utilities/addLogs.js
@@ -1,10 +1,14 @@
 const path = require('path')
 const fs = require('fs')
+const dotenv = require('dotenv')
+dotenv.config()
 
 const ensureDirectoryExists = (filePath) => {
     const dir = path.dirname(path.join(__dirname, '..', 'logs', filePath));
-    if (!fs.existsSync(dir)) {
-        fs.mkdirSync(dir, { recursive: true });
+    if (process.env.NODE_ENV !== 'production') {
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir, { recursive: true });
+        }
     }
 };
 
